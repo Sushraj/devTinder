2c0ce3455c60ef4e97a18cc8318dddd4b679a455
Sushant Rajhans <Sushantrajhans2222@gmail.com>
2025-01-05 21:34:49 +0530

    different routing methods

--- src/app.js
+++ src/app.js
@@ -20,6 +20,41 @@ app.delete('/user', (req, res) => {
 app.use("/user",(req,res) => {
     res.send("HAAAAA");
 })
+
+
+// b is optional here -> /ac , /abc
+app.get("/ab?c",(req,res) => {
+    res.send("get Data");
+})
+
+
+//  abc ,abbc, abbbbbbbbbc, you can add as many as 'b' 
+app.get("/ab+c",(req,res) => {
+    res.send("get Data");
+})
+
+
+// abcd, abSushantcd, start with 'ab' in middle anything end with 'cd'
+app.get("/ab*cd",(req,res) => {
+    res.send("get Data");
+})
+
+app.get("/a(bc)?d",(req,res) => {
+    res.send("get Data");
+});
+
+
+app.get("/getAgents",(req,res) => {
+    console.log(req.query);
+    res.send({firstName: "Sushant", lastName: "Rajhans"});
+})
+
+
+app.get("/getAgents/:agentId/:name/:password",(req,res) => {
+    console.log(req.params);
+    res.send({firstName: "Sushant", lastName: "Rajhans"});
+})
+
 app.listen(3000,() => {
     console.log("server running on port 7000");
 });
