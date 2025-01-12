01b593a925581072d843661e6282db57c42bc330
Sushant Rajhans <Sushantrajhans2222@gmail.com>
2025-01-12 14:17:05 +0530

    create few API

--- src/app.js
+++ src/app.js
@@ -3,23 +3,64 @@ const connectDB = require("./config/database");
 const app = express(); // creating new web server
 const User = require("./models/users");
 
-app.post("/signup", async (req, res) => {
-  //create new instance of User model
+// Middleware to parse JSON
+app.use(express.json());
 
-  const user = new User({
-    firstName: "virat",
-    lastName: "Kohli",
-    emailId: "virat@gmail.com",
-    password: "vk123",
-    age: 38,
-    gender: "Male",
-  });
+app.post("/signup", async (req, res) => {
+  //create new instance of User model and Access the parsed JSON data
+  const user = new User(req.body);
 
   try {
     await user.save();
     res.send("user added successfully!!");
   } catch (err) {
     res.status(400).send("Error saving the user:", +err.message);
+  }
+});
+
+//feed API - GET /feed - get all the users from the database.
+app.get("/feed", async (req, res) => {
+  try {
+    const users = await User.find();
+    if (users.length > 0) {
+      res.status(200).json({ success: true, data: users });
+    } else {
+      res.status(404).json({ success: false, message: "No profiles found" });
+    }
+  } catch (error) {
+    res
+      .status(500)
+      .json({ succes: false, message: "Server error", error: error.message });
+  }
+});
+
+app.get("/getUserByEmaiId", async (req, res) => {
+  const userEmail = req.body.emailId;
+
+  try {
+    const users = await User.find({ emailId: userEmail });
+    if (users.length === 0) {
+      res.status(404).send("User not found!");
+    } else {
+      res.send({ success: true, data: users });
+    }
+  } catch (err) {
+    res.status(400).send("something went wrong!!");
+  }
+});
+
+app.get("/getUserByEmaiIdFindOne", async (req, res) => {
+  const userEmail = req.body.emailId;
+
+  try {
+    const user = await User.findOne({ emailId: userEmail });
+    if (user.length === 0) {
+      res.status(404).send("User not found!");
+    } else {
+      res.send({ success: true, data: user });
+    }
+  } catch (err) {
+    res.status(400).send("something went wrong!!");
   }
 });
 
