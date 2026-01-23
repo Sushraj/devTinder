02c033324f06c27455d366b1aa00f91ec9f2daab
Sushant Rajhans <Sushantrajhans2222@gmail.com>
2026-01-23 12:28:56 +0530

    chat api

--- src/routes/chat.js
+++ src/routes/chat.js
@@ -1,10 +1,30 @@
 const express = require("express");
+const { Chat } = require("../models/chat");
+const { userAuth } = require("../middlewares/auth");
 const chatRouter = express.Router();
 
+chatRouter.get("/chat/:targetUserId", userAuth, async (req, res) => {
+  const { targetUserId } = req.params;
+  console.log("user request:", req.user);
+  const userId = req.user._id;
 
-chatRouter.post("/chat", async (req, res) => {
-    const {userId, targetUserId} = req.body;
-  res.send("Chat route is working");
+  try {
+    let chat = await Chat.findOne({
+      participants: { $all: [userId, targetUserId] },
+    });
+    if (!chat) {
+      chat = new Chat({
+        participants: [userId, targetUserId],
+        messages: [],
+      });
+      await chat.save();
+    }
+    res.json(chat);
+  } catch (err) {
+    res.status(400).send("ERROR : " + err.message);
+  }
 });
+
+
 
 module.exports =  chatRouter;
